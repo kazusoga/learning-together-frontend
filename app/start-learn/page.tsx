89ca9df2ad1ecac8997588e29a26098af44e7958
曽我　和久 <kazusoga1002guitar.42p67@gmail.com>
2023-07-10 14:46:03 +0900

    add: 勉強開始ページに入力欄とsubmitボタンを追加

--- app/start-learn/page.tsx
+++ app/start-learn/page.tsx
@@ -1,1 +1,27 @@
-export default function startLearn() {}
+"use client";
+
+import styles from "./styles.module.css";
+import { useState } from "react";
+
+export default function startLearn() {
+  const [title, setTitle] = useState("");
+  return (
+    <div className="text-center">
+      <h1 className={styles.pageTitle}>勉強開始</h1>
+      <input
+        className={styles.titleInput}
+        placeholder={"勉強タイトルを入力してください"}
+        type={"text"}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        className={styles.detailTextArea}
+        name=""
+        id=""
+        placeholder="勉強する内容を入力してください"
+      ></textarea>
+      <button className={styles.submitButton}>勉強開始</button>
+    </div>
+  );
+}
