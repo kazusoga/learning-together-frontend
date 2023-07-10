c56b15eb30eaac565f9d74d059f29a3badb035b6
曽我　和久 <kazusoga1002guitar.42p67@gmail.com>
2023-07-10 14:14:19 +0900

    add: header: ユーザーアイコン設置

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import Tabs from "../components/Tabs/Tabs";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div className={styles.header}>Learning Together</div>
+      <div className={styles.header}>
+        <div>Learning Together</div>
+        <div className={styles.userIcon}></div>
+      </div>
       <Tabs tabs={["勉強中", "フォロー中", "おすすめ"]} />
     </main>
   );
