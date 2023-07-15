37ae23626feb1b6acdbab999587503fc37c679e5
曽我 和久 <kazusoga1002guitar.42p67@gmail.com>
2023-07-15 15:17:22 +0900

    fix: 詳細ページ: データ取得はサーバーサイドで行う

--- app/learning-detail/[id]/page.tsx
+++ app/learning-detail/[id]/page.tsx
@@ -1,40 +1,21 @@
-'use client';
-
-import { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./styles.module.css";
 
-type Learning = {
-  id: number;
-  title: string;
-  detail: string;
-  helping: boolean;
-};
-
-export default function LearningDetail({ params }: { params: { id: string } }) {
-  const [learning, setLearning] = useState<Learning>({
-    id: 0,
-    title: "",
-    detail: "",
-    helping: false,
-  });
-
-  const fetchLearning = async () => {
-    const res = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_BASE}/learnings/${params.id}`
-    );
-    setLearning(res.data.learning);
-  };
-  useEffect(() => {
-    fetchLearning()
-  }, []);
+export default async function LearningDetail({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const learning = await axios
+    .get(`${process.env.NEXT_PUBLIC_API_BASE}/learnings/${params.id}`)
+    .then((res) => {
+      return res.data.learning;
+    });
 
   return (
     <div className={styles.learningDetail}>
       <h1 className={styles.learningDetailTitle}>{learning.title}</h1>
-      <div className={styles.learningDetailContent}>
-        {learning.detail}
-      </div>
+      <div className={styles.learningDetailContent}>{learning.detail}</div>
     </div>
   );
 }
