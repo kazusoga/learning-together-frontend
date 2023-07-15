e8b925afa276f0fbc76d9c18de01657aedc6809a
曽我 和久 <kazusoga1002guitar.42p67@gmail.com>
2023-07-15 14:06:24 +0900

    fix: 詳細画面に情報を表示

--- app/learning-detail/[id]/page.tsx
+++ app/learning-detail/[id]/page.tsx
@@ -1,5 +1,40 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import axios from "axios";
 import styles from "./styles.module.css";
 
+type Learning = {
+  id: number;
+  title: string;
+  detail: string;
+  helping: boolean;
+};
+
 export default function LearningDetail({ params }: { params: { id: string } }) {
-  return <div>LearningDetail {params.id}</div>;
+  const [learning, setLearning] = useState<Learning>({
+    id: 0,
+    title: "",
+    detail: "",
+    helping: false,
+  });
+
+  const fetchLearning = async () => {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_BASE}/learnings/${params.id}`
+    );
+    setLearning(res.data.learning);
+  };
+  useEffect(() => {
+    fetchLearning()
+  }, []);
+
+  return (
+    <div className={styles.learningDetail}>
+      <h1 className={styles.learningDetailTitle}>{learning.title}</h1>
+      <div className={styles.learningDetailContent}>
+        {learning.detail}
+      </div>
+    </div>
+  );
 }
