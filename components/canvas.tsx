02f475d7b1b66fd70e62d91f1d57dd63c4bc0dd7
曽我 和久 <kazusoga1002guitar.42p67@gmail.com>
2023-07-29 17:21:43 +0900

    add: 消しゴム機能追加

--- components/canvas.tsx
+++ components/canvas.tsx
@@ -1,99 +1,31 @@
 "use client";
 
-import { use, useEffect } from "react";
+import { useRef, useEffect } from "react";
+
+let canvas: HTMLCanvasElement;
+
+let x: number = 0;
+let y: number = 0;
+let x2: number = 0;
+let y2: number = 0;
+
+let isDown: boolean = false;
+
+let cursorType: "pencil" | "eraser" = "pencil";
+
+let ctx: CanvasRenderingContext2D | null;
 
 export default function Canvas({ channels }: { channels: RTCDataChannel[] }) {
   console.log("canvasがレンダリングされました！");
-  let ctx: CanvasRenderingContext2D;
-  let canvas: HTMLCanvasElement;
-  let mouseDown: (e: MouseEvent) => void;
-  let mouseMove: (e: MouseEvent) => void;
-  let mouseUp: () => void;
-  let mouseOut: () => void;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const intializeCanvas = () => {
     // キャンバスのサイズを設定する
-    canvas = document.getElementById("whiteboard") as HTMLCanvasElement;
+    canvas = canvasRef.current;
     canvas.width = 500;
     canvas.height = 500;
 
-    // ホワイトボードにマウスで描画できるようにする
     ctx = canvas.getContext("2d");
-
-    // マウスの座標を取得する
-    let x: number;
-    let y: number;
-
-    // マウスが押されているかどうか
-    let isDown = false;
-
-    // マウスが押された時の処理
-    mouseDown = (e: MouseEvent) => {
-      // マウスが押されている状態にする
-      isDown = true;
-
-      // マウスの座標を取得する
-      x = e.offsetX;
-      y = e.offsetY;
-    };
-
-    // マウスが動いた時の処理
-    mouseMove = (e: MouseEvent) => {
-      // マウスが押されていなければ処理を中断する
-      if (!isDown) return;
-
-      // マウスの座標を取得する
-      const x2 = e.offsetX;
-      const y2 = e.offsetY;
-
-      // 線を描画する
-      ctx.beginPath();
-      console.log("描画開始点", x, y);
-      ctx.moveTo(x, y);
-      ctx.lineTo(x2, y2);
-      console.log("描画終了点", x2, y2);
-      ctx.stroke();
-
-      // 通信相手に送信する
-      console.log("channel はありますか！", channels);
-      if (channels) {
-        channels.forEach((channel) => {
-          if (channel.readyState !== "open") {
-            console.log("channel は開いていません！");
-            // マウスの座標を更新する
-            x = x2;
-            y = y2;
-            return;
-          }
-
-          console.log("いけえええええええ！！！");
-          channel.send(JSON.stringify({ x, y, x2, y2 }));
-          console.log("送信しました！");
-        });
-      }
-
-      // マウスの座標を更新する
-      x = x2;
-      y = y2;
-    };
-
-    // マウスが離れた時の処理
-    mouseUp = () => {
-      // マウスが押されていない状態にする
-      isDown = false;
-    };
-
-    // マウスがキャンバスから離れた時の処理
-    mouseOut = () => {
-      // マウスが押されていない状態にする
-      isDown = false;
-    };
-
-    // イベントを登録する
-    canvas.addEventListener("mousedown", mouseDown);
-    canvas.addEventListener("mousemove", mouseMove);
-    canvas.addEventListener("mouseup", mouseUp);
-    canvas.addEventListener("mouseout", mouseOut);
   };
 
   useEffect(() => {
@@ -102,16 +34,120 @@ export default function Canvas({ channels }: { channels: RTCDataChannel[] }) {
     intializeCanvas();
   }, [channels]);
 
-  // イベントを削除する
-  useEffect(() => {
-    return () => {
-      console.log("canvas がアンマウントされました！");
-      canvas.removeEventListener("mousedown", mouseDown);
-      canvas.removeEventListener("mousemove", mouseMove);
-      canvas.removeEventListener("mouseup", mouseUp);
-      canvas.removeEventListener("mouseout", mouseOut);
-    };
-  }, []);
+  const changeCursorToPencil = () => {
+    console.log("ペンを選択しました！");
+    canvas.style.cursor = "default";
 
-  return <canvas id="whiteboard"></canvas>;
+    cursorType = "pencil";
+  };
+
+  const changeCursorToEraser = () => {
+    console.log("消しゴムを選択しました！");
+    canvas.style.cursor = "pointer";
+
+    cursorType = "eraser";
+  };
+
+  const mouseDown: React.MouseEventHandler<HTMLCanvasElement> = (
+    e: React.MouseEvent<HTMLCanvasElement, globalThis.MouseEvent>
+  ) => {
+    // マウスが押されている状態にする
+    isDown = true;
+
+    // マウスの座標を取得する
+    console.log(
+      "マウスが押されました！",
+      e.nativeEvent.offsetX,
+      e.nativeEvent.offsetY
+    );
+    x = e.nativeEvent.offsetX;
+    y = e.nativeEvent.offsetY;
+  };
+
+  // マウスが動いた時の処理
+  const mouseMove: React.MouseEventHandler = (e) => {
+    // マウスが押されていなければ処理を中断する
+    if (!isDown) return;
+
+    // マウスの座標を取得する
+    x2 = e.nativeEvent.offsetX;
+    y2 = e.nativeEvent.offsetY;
+
+    if (cursorType === "pencil") {
+      // 線を描画する
+      ctx.beginPath();
+      console.log("描画開始点", x, y);
+      ctx.moveTo(x, y);
+      ctx.lineTo(x2, y2);
+      console.log("描画終了点", x2, y2);
+      ctx.stroke();
+    } else if (cursorType === "eraser") {
+      // 消しゴムを描画する
+      ctx.clearRect(x2, y2, 10, 10);
+    }
+
+    // 通信相手に送信する
+    console.log("channel はありますか！", channels);
+    if (channels) {
+      channels.forEach((channel) => {
+        if (channel.readyState !== "open") {
+          console.log("channel は開いていません！");
+          // マウスの座標を更新する
+          x = x2;
+          y = y2;
+          return;
+        }
+
+        console.log("いけえええええええ！！！");
+        channel.send(JSON.stringify({ x, y, x2, y2 }));
+        console.log("送信しました！");
+      });
+    }
+
+    // マウスの座標を更新する
+    x = x2;
+    y = y2;
+  };
+
+  // マウスが離れた時の処理
+  const mouseUp = () => {
+    // マウスが押されていない状態にする
+    isDown = false;
+  };
+
+  // マウスがキャンバスから離れた時の処理
+  const mouseOut = () => {
+    // マウスが押されていない状態にする
+    isDown = false;
+  };
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        onMouseDown={mouseDown}
+        onMouseMove={mouseMove}
+        onMouseUp={mouseUp}
+        onMouseOut={mouseOut}
+      ></canvas>
+      {/* ペンか消しゴムかを選択する欄を作る */}
+      <div>
+        <input
+          type="radio"
+          name="tool"
+          value="pencil"
+          onClick={changeCursorToPencil}
+          defaultChecked
+        />
+        ペン
+        <input
+          type="radio"
+          name="tool"
+          value="eraser"
+          onClick={changeCursorToEraser}
+        />
+        消しゴム
+      </div>
+    </div>
+  );
 }
