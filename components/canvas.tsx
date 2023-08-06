c2f94bff57713f1fea427e2d4b6f2fec86b70172
曽我 和久 <kazusoga1002guitar.42p67@gmail.com>
2023-08-06 19:25:33 +0900

    add: 描画情報を永続化する

--- components/canvas.tsx
+++ components/canvas.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useRef, useEffect, use } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 let canvas: HTMLCanvasElement;
 
@@ -27,6 +29,38 @@ export default function Canvas({ channels }: { channels: RTCDataChannel[] }) {
     canvas.height = 500;
 
     ctx = canvas.getContext("2d");
+
+    // firestore から描画情報を取得する
+    const db = firebase.firestore();
+    db.collection("whiteboards")
+      .doc("5")
+      .collection("points")
+      .doc("latest")
+      .onSnapshot((doc) => {
+        console.log("firestore から初期描画データを取得しました！", doc.data());
+
+        // 描画情報を取得する
+        const data = doc.data();
+
+        // 描画情報をキャンバスに反映する
+        if (!data) {
+          return;
+        }
+        if (!ctx) {
+          return;
+        }
+
+        for (const point of Object.values(data)) {
+          if (point.type === "pencil") {
+            ctx!.beginPath();
+            ctx.moveTo(point.startX, point.startY);
+            ctx.lineTo(point.endX, point.endY);
+            ctx.stroke();
+          } else if (point.type === "eraser") {
+            ctx.clearRect(point.x2, point.y2, 10, 10);
+          }
+        }
+      });
   };
 
   useEffect(() => {
@@ -65,6 +99,34 @@ export default function Canvas({ channels }: { channels: RTCDataChannel[] }) {
     y = e.nativeEvent.offsetY;
   };
 
+  const sendLatestPointToFirestore = (
+    type: "pencil" | "eraser",
+    startX: number,
+    startY: number,
+    endX: number,
+    endY: number
+  ) => {
+    // firestore に最新の座標を送信して追加する
+    const db = firebase.firestore();
+
+    // 連番を作成する
+    const id = Date.now().toString();
+
+    // id は変数展開する。{id: {x, y}} という形で保存する
+    // 前のものも残しておく
+    db.collection("whiteboards")
+      .doc("5")
+      .collection("points")
+      .doc("latest")
+      .set({ [id]: { type, startX, startY, endX, endY } }, { merge: true })
+      .then(() => {
+        console.log("最新の座標を firestore に送信しました！");
+      })
+      .catch((error) => {
+        console.log("最新の座標の送信に失敗しました！", error);
+      });
+  };
+
   // マウスが動いた時の処理
   const mouseMove: React.MouseEventHandler = (e) => {
     // マウスが押されていなければ処理を中断する
@@ -84,9 +146,13 @@ export default function Canvas({ channels }: { channels: RTCDataChannel[] }) {
       ctx.lineTo(x2, y2);
       console.log("描画終了点", x2, y2);
       ctx.stroke();
+
+      // firestore に描画情報を送信する
+      sendLatestPointToFirestore("pencil", x, y, x2, y2);
     } else if (cursorType === "eraser") {
       // 消しゴムを描画する
       ctx.clearRect(x2, y2, 10, 10);
+      sendLatestPointToFirestore("eraser", x, y, x2, y2);
     }
 
     // 通信相手に送信する
