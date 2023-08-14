86c63891e171444a9957825c84b4298c8975317c
曽我 和久 <kazusoga1002guitar.42p67@gmail.com>
2023-08-14 14:55:40 +0900

    fix: ヘッダーのアイコンに hover するとログアウトボタンが表示されるようにした

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,18 +4,47 @@ import Link from "next/link";
 import styles from "./styles.module.css";
 import Logout from "../logout";
 
+import { useState } from "react";
+import { Menu, MenuItem } from "@mui/material";
+
 import { useloginUserContext } from "@/app/providers";
 
 export default function Header() {
   const loginUser = useloginUserContext();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => setAnchorEl(null);
 
   return (
     <div className={styles.header}>
       <Link href="/">Learning Together</Link>
       {loginUser && (
         <>
-          <div className={styles.userIcon}></div>
-          <Logout />
+          <Menu
+            id="menu-app-bar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            MenuListProps={{ onMouseLeave: handleClose }}
+          >
+            <MenuItem>
+              <Logout />
+            </MenuItem>
+          </Menu>
+          <div className={styles.userIcon} onMouseOver={handleMenu}></div>
         </>
       )}
       {!loginUser && (
